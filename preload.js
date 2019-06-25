8a232449a4203bf0f3e5ee7570e2c90ebe82d025
Bitixel <info@bitixel.com>
2019-06-25 18:36:33 +0530

    updating to electron 5

---
+++ preload.js
@@ -0,0 +1,10 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    for (const versionType of['chrome', 'electron', 'node']) {
+        document.getElementById(`${versionType}-version`).innerText = process.versions[versionType]
+    }
+
+    document.getElementById('serialport-version').innerText = require('serialport/package').version
+
+})
