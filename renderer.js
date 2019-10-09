3b3a5ba8c8b38ed78b51349f763ce7d0796149b2
Spenser Saling <spenser0saling@gmail.com>
2019-10-09 16:36:11 -0700

    Switch to tableify
    Switch from "data-table" to "tableify" for creating table html from the ports array.

--- renderer.js
+++ renderer.js
@@ -3,7 +3,7 @@
 // All of the Node.js APIs are available in this process.
 
 const serialport = require('serialport')
-const createTable = require('data-table')
+const tableify = require('tableify')
 
 serialport.list((err, ports) => {
   console.log('ports', ports);
@@ -18,11 +18,6 @@ serialport.list((err, ports) => {
     document.getElementById('error').textContent = 'No ports discovered'
   }
 
-  const headers = Object.keys(ports[0])
-  const table = createTable(headers)
-  tableHTML = ''
-  table.on('data', data => tableHTML += data)
-  table.on('end', () => document.getElementById('ports').innerHTML = tableHTML)
-  ports.forEach(port => table.write(port))
-  table.end();
+  tableHTML = tableify(ports)
+  document.getElementById('ports').innerHTML = tableHTML
 })
