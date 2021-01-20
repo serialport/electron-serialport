0cad5d97f7d1ead1e983b16e9e5b49feb1e6792d
Stephen Murphy <stephendpmurphy@msn.com>
2021-01-20 11:30:55 -0500

    Updating Electron & Serialport versions (#7)
    * Updating Electron & Serialport versions as well as updating js for retrieving port list
    * Updating url and package.json version
    * Check for ports every 2s so a device can be plugged in and shown AFTER the app has been started

--- renderer.js
+++ renderer.js
@@ -5,19 +5,28 @@
 const serialport = require('serialport')
 const tableify = require('tableify')
 
-serialport.list((err, ports) => {
-  console.log('ports', ports);
-  if (err) {
-    document.getElementById('error').textContent = err.message
-    return
-  } else {
-    document.getElementById('error').textContent = ''
-  }
+async function listSerialPorts() {
+  await serialport.list().then((ports, err) => {
+    if(err) {
+      document.getElementById('error').textContent = err.message
+      return
+    } else {
+      document.getElementById('error').textContent = ''
+    }
+    console.log('ports', ports);
 
-  if (ports.length === 0) {
-    document.getElementById('error').textContent = 'No ports discovered'
-  }
+    if (ports.length === 0) {
+      document.getElementById('error').textContent = 'No ports discovered'
+    }
 
-  tableHTML = tableify(ports)
-  document.getElementById('ports').innerHTML = tableHTML
-})
+    tableHTML = tableify(ports)
+    document.getElementById('ports').innerHTML = tableHTML
+  })
+}
+
+// Set a timeout that will check for new serialPorts every 2 seconds.
+// This timeout reschedules itself.
+setTimeout(function listPorts() {
+  listSerialPorts();
+  setTimeout(listPorts, 2000);
+}, 2000);
